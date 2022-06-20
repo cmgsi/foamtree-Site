89479fb2d1b5ba9f6cb9b86ae18e62533ea18fd4
cmgsi <cmgsi@hotmail.com>
2022-06-20 19:44:50 -0300

    commit update

--- src/Queries.js
+++ src/Queries.js
@@ -1,6 +1,31 @@
+import execAllViz from "./carrotsearch/oldSite/lib/d3SOBr.js";
+
+import jquery from  "./carrotsearch/oldSite/lib/jquery/jquery.min.js"
+import d3 from "./carrotsearch/oldSite/lib/d3/d3.v3.min.js"
+import d3SOBr from "./carrotsearch/oldSite/lib/d3SOBr.js"
+import d3sparql from "./carrotsearch/oldSite/d3sparql.js"
+ 
 export default function Queries() {
     return (
         <div className="Queries">
+
+
+
+
+
+            
+            <script>
+                $( document ).ready(function() {
+
+                    execAllViz(getAllPageParams())
+                });
+
+            </script>
+
+
+
+
+
             <p>Escolha a origem dos dados:
                 <select id="selectEstado" onchange="toggleMunicipio()">
                     <option value="SP">
